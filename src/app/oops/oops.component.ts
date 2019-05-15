bea6f256e7b06292a82dd2f28c75ad2931cd3a9c
Aditya <adi.shirsat3004@gmail.com>
2019-05-15 17:28:27 +0530

    first commit

--- src/app/oops/oops.component.ts
+++ src/app/oops/oops.component.ts
@@ -15,9 +15,10 @@ export class OopsComponent implements OnInit {
   list: any[];
   strings: any[];
   multiArray:any[];
-  tempArray:any[];
+  tempArray = [];
   control;
   refinedItems=[];
+  sortingElem: [];
   
 
 
@@ -38,114 +39,53 @@ export class OopsComponent implements OnInit {
         id: 1,
         title: 'Gmail',
         checked:false,
-        order:1
+        order:1,
+        url: ''
       },
       {
         id:2,
         title: 'Facebook',
         checked:false,
-        order:2
+        order:2,
+        url: ''
 
       },
       {
         id:3,
         title: 'LinkedIn',
         checked:false,
-        order:3
+        order:3,
+        url: ''
 
       },
       {
         id:4,
         title: 'Github',
         checked:false,
-        order:4
+        order:4,
+        url: ''
 
       },
       {
         id:5,
         title: 'Orkut',
         checked:false,
-        order:5
-
-      },
-    ],
-    this.strings = [],
-    this.multiArray=[
-      {
-        id: 1,
-        title: 'Gmail',
-        checked: false,
-        order:1
-      },
-      {
-        id:2,
-        title: 'Facebook',
-        checked: false,
-        order:2
-
-      },
-      {
-        id:3,
-        title: 'LinkedIn',
-        checked: false,
-        order:3
-
-      },
-      {
-        id:4,
-        title: 'Github',
-        checked: false,
-        order:4
-
-      },
-      {
-        id:5,
-        title: 'Orkut',
-        checked: false,
-        order:5
+        order:5,
+        url: ''
 
       },
     ]
   }
 
-addItems(obj){
-  debugger;
-  console.log('obj',obj)
-  //this.sortarrayService.accordians.push(obj);
-  const filteredArray=this.list.filter((item) => {
-    return item.id === obj.id
-  })[0];
-  console.log('filtered', filteredArray)
-  
-  
-  if (obj.checked) {
-  this.sortarrayService.accordians.push(filteredArray);
-}
-else{
-//this.sortarrayService.accordians.pop()
-for (var i =0; i < this.sortarrayService.accordians.length; i++)
-   if (this.sortarrayService.accordians[i].checked === false) {
-    this.sortarrayService.accordians.splice(i,1);
-      break;
-   }
-}
-  // const tempArray=[];
-
-  // if (obj.checked) {
-  //    tempArray.push(filteredArray);
-  // }
-  // else{
-  //   this.tempArray.pop();
-  // }
-  // const sortedArr = this.sortarrayService.sortedArr(tempArray);
-  // console.log('sorted arry i m here', sortedArr);
-
-  //this.result();
-  //console.log(tempArray);
-  //const sortedArr = this.sortarrayService.sortedArr(this.sortarrayService.accordians);
-  //console.log('sorted arry i m here', sortedArr);
-  console.log(filteredArray)
-  console.log(this.list);
+addItems(obj){  
+  const index = this.tempArray.findIndex(matchObj => {
+    return matchObj['id'] === obj.id;
+  });
+  console.log('index', index)
+ obj.checked ? this.tempArray.push(obj) : this.tempArray.splice(index, 1);
+  const sortedArr = this.sortarrayService.sortedArr(this.tempArray);
+  console.log('sorted arry i m here', sortedArr);
+  this.sortingElem = sortedArr;
 }
 
 get result(){
@@ -155,46 +95,9 @@ return sortedArr;
 
 
 
-
-//this.resultArray = this.sortarrayService.sorted(dfa);
-get sorting() {
-  return this.list.filter(item => item.checked);
-  // this.sortarrayService.sorted.filter((element, index) => {
-  //   return index === 0 || element.number !== this.sortarrayService.accordians[index-1].number;
-  // });
-  // return this.sortarrayService.accordians;
-}
-
-
   onSend(value) {
     this.strings.push(value + "-" + this.email);
     console.log(this.strings);
     this.email = "";
   }
-
-   
-  // ngOnInit() {
-  //   const person1= new Oops('aditya','shirsat',30-4-1992)
-  //   console.log(person1);
-  //   const person2= new Oops('sushant','rajhans',15-6-1995)
-  //   console.log(person2);
-  //   console.log(person2.firstName);
-  //   console.log(person2.dob);
-  // }
-
-
-
 }
-
-
-// if (obj.checked) {
-//   this.sortarrayService.accordians.push(filteredArray);
-// }
-// else{
-//  this.sortarrayService.accordians.pop();
-// }
-
-// get result(){
-//   const sortedArr = this.sortarrayService.sortedArr(this.sortarrayService.accordians);
-// return sortedArr;
-// }
